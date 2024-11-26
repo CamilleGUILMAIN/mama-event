db9899bfca7d089b1407fad7c85c7b5e03c890f0
Jean Delarbre <delarbrejean@icloud.com>
2024-11-26 17:36:18 +0100

    en cours

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,11 +1,29 @@
 class EventController < ApplicationController
   def new
     @event = Event.new
+    raise!
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.date = Date.parse(params[:events][:date])
+    if @event.save
+      redirect_to events_path
+    else
+      render "event/new",status: :unprocessable_entity
+    end
+  end
+
+  def index
+    @events = Event.all
   end
 
   def show
   end
 
   private
+  def event_params
+    params.require(:event).permit(:name, :description, :date)
+  end
 
 end
