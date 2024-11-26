9456b0fec0dff75a84401e27526a7807d4a19ba5
Vincent Demaret <demaretvincent001@gmail.com>
2024-11-26 18:31:14 +0100

    add boiler-plate

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'event/new'
-  get 'event/show'
   devise_for :users
-  resources :charities
+  root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,6 +9,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  # 
-  resources :events
 end
